bcaec94b38c1098ea9cefeb48b2c8183774406f0
Admin <nganlehongbao@gmail.com>
2022-04-01 02:15:08 +0700

    01-04 fix course update2

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -8,7 +8,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { courseCreate } from '../model/courseC';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 
@@ -42,11 +42,16 @@ export class CourseListComponent implements OnInit {
   optionPlatform: string = '';
 
   listStudent: any[] = [];
+  courseDetailForm!: FormGroup;
 
-  public disableInputText = true;
   public display_chooseSkill= false;
   public skilldb: any;
   public selectSkills ={
+    skill_id: [] as any,
+    skill_name: [] as any
+  }
+
+  public newSkills ={
     skill_id: [] as any,
     skill_name: [] as any
   }
@@ -54,7 +59,11 @@ export class CourseListComponent implements OnInit {
   public updateCourse = {
     course_id: '',
     course_name: '',
-    skills: [],
+    skills : {
+      skill_id: [] as any,
+      skill_name: [] as any
+    },
+    skillFlat: '',
     platform: '',
     category: '',
     totalLength: ''
@@ -66,12 +75,19 @@ export class CourseListComponent implements OnInit {
 
   course_category: any;
   constructor(private course: CourseService,
+    private _formBuilder: FormBuilder,
     private employeeService: EmployeeService,
     private router: Router,
     public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
+    this.courseDetailForm = new FormGroup({
+      course_name: new FormControl(),
+      platform: new FormControl(),
+      category: new FormControl(),
+      course_length: new FormControl()
+    });
     this.GetAllInfo();
   }
 
@@ -107,17 +123,58 @@ export class CourseListComponent implements OnInit {
   }
 
   courseDetail(c:any){
+    //reset newskill
+    this.newSkills = {
+      skill_id : [],
+      skill_name : []
+    };
+    //reset alert
+    var alert = document.getElementById('alert-success') as HTMLElement;
+    alert.setAttribute("style", "display: none");
+    var alert = document.getElementById('alert-fail') as HTMLElement;
+    alert.setAttribute("style", "display: none");
+    //
+    this.enableButton(false);
+    this.enableInputText(false);
+    //reset skills
+    this.selectSkills ={
+      skill_id : [],
+      skill_name : []
+    };
+
     //reset update course
     this.updateCourse = {
       course_id: c.id,
       course_name: '',
-      skills: [],
+      skills : {
+        skill_id: [] as any,
+        skill_name: [] as any
+      },
+      skillFlat: '',
       platform: '',
       category: '',
       totalLength: ''
     }
 
     this.data = c;
+
+    this.courseDetailForm.controls['course_name'].setValue(c.name);
+    this.courseDetailForm.controls['platform'].setValue(c.platform);
+    this.courseDetailForm.controls['category'].setValue(c.category);
+    this.courseDetailForm.controls['course_length'].setValue(c.courseLength);
+    
+    // for (let index = 0; index < c.skills.length; index++) {
+    //   const element = c.skills[index];// skill name
+
+    //   var id = this.skilldb.filter((x: { name: string; }) => x.name == element)[0].id;
+    //   var find_id = this.selectSkills.skill_id.filter((x: string) => x == id); 
+    //   if(find_id.length==0){
+    //     this.selectSkills.skill_id.push(id);
+    //     this.selectSkills.skill_name.push(element);
+    //   }
+    //   console.log('id', id);
+    // }
+
   }
 
   showListStudent(data: any){
@@ -195,7 +252,8 @@ export class CourseListComponent implements OnInit {
     // var name_update = document.getElementById('name_update') as HTMLElement;
     // name_update.setAttribute("style", "display: block");
 
-
+    this.enableInputText(true);
+    this.enableButton(true);
 
     this.changeStyle(true);
   }
@@ -204,9 +262,7 @@ export class CourseListComponent implements OnInit {
     var str = value ? 'none':'block';
     var str2 = value ? 'block':'none';
 
-    // ẩn hiện input text và btn add
-    this.disableInputText = !this.disableInputText;
-    this.display_chooseSkill = !this.display_chooseSkill;
+    
     // ẩn btn chỉnh sửa, hiện btn lưu
     var editBtn = document.getElementById('edit') as HTMLElement;
     editBtn.setAttribute("style", "display: " + str);
@@ -228,13 +284,32 @@ export class CourseListComponent implements OnInit {
       element.checked = false;
     });
 
+    
+
     //check các skills cũ của course
     for (let index = 0; index < skillsCourse.length; index++) {
       const element = skillsCourse[index];
       myCheckboxes[this.skilldb.findIndex((x: { name: string; }) => x.name == element)].checked = true;
-    }
 
-    //check các skills mới của course (nếu đã chọn)
+      //push các skill vào list selectSkills
+      // var id = this.skilldb.filter((x: { name: string; }) => x.name == element)[0].id;
+      // this.selectSkills.skill_id.push(id);
+      // this.selectSkills.skill_name.push(element);
+
+
+      var id = this.skilldb.filter((x: { name: string; }) => x.name == element)[0].id;
+      var find_id = this.selectSkills.skill_id.filter((x: string) => x == id); 
+      if(find_id.length==0){
+        this.selectSkills.skill_id.push(id);
+        this.selectSkills.skill_name.push(element);
+      }
+
+      console.log('id', id);
+    }
+    console.log('id', this.selectSkills);
+
+
+    // check các skills mới của course (nếu đã chọn)
     for (let index = 0; index < this.selectSkills.skill_id.length; index++) {
       const element = this.selectSkills.skill_id[index];
       myCheckboxes[this.skilldb.findIndex((x: { id: string; }) => x.id == element)].checked = true;
@@ -249,30 +324,7 @@ export class CourseListComponent implements OnInit {
   }
 
   submitSkills(){
-    //reset select skills
-    this.selectSkills ={
-      skill_id : [],
-      skill_name : []
-    } 
-
-    //ẩn modal coursedetail hiện modal chọn skill
-    $('#popup3').modal('toggle');
-    $('#chooseSkills').modal('hide');
-    
-    //lấy ra list các skills (chỉ có id) đã được check
-    let myCheckboxes = this.myCheckboxes.toArray();
-    var checkedSkills = myCheckboxes.filter(checkbox => checkbox.checked == true).map(a => a.value);
-    
-    //lấy ra list các skill (id, name) đã được check
-    this.skilldb.forEach((element: { id: string; name: string }) => {
-      if(checkedSkills.find(a => a == element.id)){
-        this.selectSkills.skill_id.push(element.id);
-        this.selectSkills.skill_name.push(element.name);
-      }
-    });
-
-    console.log('checked', this.selectSkills);
-
+    console.log('submit');
     //ẩn các skill cũ của khoá
     var skill_data = document.getElementsByClassName('skill_data');
     for (let index = 0; index < skill_data.length; index++) {
@@ -286,10 +338,80 @@ export class CourseListComponent implements OnInit {
       const element = update_skill[index];
       element.setAttribute("style", "display: block");
     }
+    
+    console.log('checked', this.selectSkills);
+
+     //ẩn modal coursedetail hiện modal chọn skill
+     $('#chooseSkills').modal('hide');
+     $('#popup3').modal('toggle');
+     
+  }
+
+  
+  enableInputText(key: boolean){
+    if(!key){
+      this.courseDetailForm.controls['course_name'].disable();
+      this.courseDetailForm.controls['category'].disable();
+      this.courseDetailForm.controls['platform'].disable();
+      this.courseDetailForm.controls['course_length'].disable();
+    }else{
+      this.courseDetailForm.controls['course_name'].enable();
+      this.courseDetailForm.controls['category'].enable();
+      this.courseDetailForm.controls['platform'].enable();
+      this.courseDetailForm.controls['course_length'].enable();
+    }
+    //disable text
+    
+  }
+
+  enableButton(key: boolean){
+    if(!key){
+      this.display_chooseSkill = false;
+    }else{
+      this.display_chooseSkill = true;
+    }
   }
 
   SaveCourse(){
-    this.updateCourse.skills = this.selectSkills.skill_id;
+    
+    this.enableInputText(false);
+    this.enableButton(false);
+
+    //flat la do dai cua select skill
+    var flat = this.selectSkills.skill_id.length;
+
+    var skills = {
+      skill_id: [] as any,
+      skill_name: [] as any
+    }
+
+    //push selectSkill vao skill
+    this.selectSkills.skill_id.forEach((element: number) => {
+      skills.skill_id.push(element);
+    });
+    this.selectSkills.skill_name.forEach((element: number) => {
+      skills.skill_name.push(element);
+    });
+
+    //push newSkill vao skill
+    this.newSkills.skill_id.forEach((element: number) => {
+      skills.skill_id.push(element);
+    });
+    this.newSkills.skill_name.forEach((element: any) => {
+      skills.skill_name.push(element);
+    });
+
+    console.log('flat', flat);
+    console.log('skill', skills);
+
+    //
+    this.updateCourse.course_name = this.courseDetailForm.controls['course_name'].value;
+    this.updateCourse.platform = this.courseDetailForm.controls['platform'].value;
+    this.updateCourse.category = this.courseDetailForm.controls['category'].value;
+    this.updateCourse.totalLength = this.courseDetailForm.controls['course_length'].value;
+    this.updateCourse.skills = skills;
+    this.updateCourse.skillFlat = flat;
+
     console.log('coursePlatform', this.updateCourse);
     this.changeStyle(false);
 
@@ -306,6 +428,54 @@ export class CourseListComponent implements OnInit {
         alert.setAttribute("style", "display: block");
       }
     )
+  }
+
+
+  checkSkills(s: any, event: MatCheckboxChange){
+    if(event.checked == true){
+      this.selectSkills.skill_id.push(s.id);
+      this.selectSkills.skill_name.push(s.name);
+    }
+    else{
+      this.selectSkills.skill_id = this.selectSkills.skill_id.filter((obj: any) => obj !== s.id); 
+      this.selectSkills.skill_name = this.selectSkills.skill_name.filter((obj: any) => obj !== s.name);
+    }
+    console.log('s', this.selectSkills);
+  }
+
+  public skillIdCount = 1;
+  public newSkillName: any;
+  addSkill() {
+    this.newSkills.skill_id.push(Math.max(...this.skilldb.map((o: { id: string; }) => o.id), 0)+this.skillIdCount);
+    this.skillIdCount++;
+    this.newSkills.skill_name.push(this.newSkillName);
+
+    console.log("newskill", this.newSkills.skill_id, this.newSkills.skill_name);
+
+    this.newSkillName = '';
+  }
+
+  deleteSkill(id: number){
+    for(let i=0; i< this.selectSkills.skill_id.length; i++){
+      if(i == id){
+        var id_value = this.selectSkills.skill_id.splice(i, 1);
+        this.selectSkills.skill_name.splice(i, 1);
+        
+        let myCheckboxes = this.myCheckboxes.toArray();
+        myCheckboxes[this.skilldb.findIndex((x: { id: string; }) => x.id == id_value)].checked = false;
+      }
+    }
+    //const index = this.sk.skill_id.findIndex(si => si === id);
+
+  }
+
+  deleteSkill2(id: number){
+    for(let i=0; i< this.newSkills.skill_id.length; i++){
+      if(i == id){
+        this.newSkills.skill_id.splice(i, 1);
+        this.newSkills.skill_name.splice(i, 1);
+      }
+    }
   }
 
 }
@@ -356,7 +526,6 @@ export class CourseCreateComponent implements OnInit {
       category: ['', Validators.required],
       image: ['', Validators.required],
       courseLength: ['', Validators.required],
-
     });
     this.secondFormGroup = this._formBuilder.group({
       newName: [''],
